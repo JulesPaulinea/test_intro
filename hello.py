3702d6a283fde5976aba8812453f3d1e4276c591
Jules PAULINEAU <jjpaulineau@gmail.com>
2023-02-27 14:34:56 +0100

    new year change

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
 def say_hello(b):
-    print(f'hello world {b}')
+    say_sts('hello world', b)
+
+def say_sts(one, thing):
+    print(one,thing, sep=' ')
+
+def happy_new_year(name):
+    say_sts("happy_new_year", 'Bob')
--- hello_funs.py
+++ hello_funs.py
@@ -1,3 +1,5 @@
 import hello
 
 hello.say_hello(' Uncle Bob')
+
+hello.say_sts('Happy new year', 'Bob')
